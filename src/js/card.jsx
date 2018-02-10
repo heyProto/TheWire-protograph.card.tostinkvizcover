11ab3b1d47fe53dc944e21c3b7aca569f548b2cb
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2018-02-10 15:33:36 +0530

    added logic to take numbers dynamically

--- src/js/card.jsx
+++ src/js/card.jsx
@@ -69,7 +69,7 @@ export default class toStinkCoverVizCard extends React.Component {
             }
         });
       });
-    },100)
+    },1000)
   }
 
   getLanguageTexts(languageConfig) {
@@ -104,27 +104,31 @@ export default class toStinkCoverVizCard extends React.Component {
         killed_arr = [],
         convicted_arr = []
 
-      console.log(employed_arr, killed_arr, convicted_arr, "employed_arr")
+      let employed = this.insertZero(employed_count, employed_arr),
+        killed = this.insertZero(killed_count, killed_arr),
+        convicted = this.insertZero(convicted_count, convicted_arr)
+
+      console.log(employed, killed,convicted, "employed_arr")
       return(
         <div className="ms-cover">
           <img className="col16-banner" src={data.banner_image.desktop}/>
           <div className="employed-counter counter">
-            <div className="animate-count single-counter ec-1">2</div>
-            <div className="animate-count single-counter ec-2">4</div>
-            <div className="animate-count single-counter ec-3">0</div>
-            <div className="animate-count single-counter ec-4">4</div>
+            <div className="animate-count single-counter ec-1">{employed[0]}</div>
+            <div className="animate-count single-counter ec-2">{employed[1]}</div>
+            <div className="animate-count single-counter ec-3">{employed[2]}</div>
+            <div className="animate-count single-counter ec-4">{employed[3]}</div>
           </div>
           <div className="killed-counter counter">
-            <div className="animate-count single-counter kc-1">0</div>
-            <div className="animate-count single-counter kc-2">3</div>
-            <div className="animate-count single-counter kc-3">6</div>
-            <div className="animate-count single-counter kc-4">6</div>
+            <div className="animate-count single-counter kc-1">{killed[0]}</div>
+            <div className="animate-count single-counter kc-2">{killed[1]}</div>
+            <div className="animate-count single-counter kc-3">{killed[2]}</div>
+            <div className="animate-count single-counter kc-4">{killed[3]}</div>
           </div>
           <div className="convicted-counter counter">
-            <div className="animate-count single-counter cc-1">0</div>
-            <div className="animate-count single-counter cc-2">0</div>
-            <div className="animate-count single-counter cc-3">0</div>
-            <div className="animate-count single-counter cc-4">2</div>
+            <div className="animate-count single-counter cc-1">{convicted[0]}</div>
+            <div className="animate-count single-counter cc-2">{convicted[1]}</div>
+            <div className="animate-count single-counter cc-3">{convicted[2]}</div>
+            <div className="animate-count single-counter cc-4">{convicted[3]}</div>
           </div>
         </div>
       )
@@ -136,29 +140,40 @@ export default class toStinkCoverVizCard extends React.Component {
       return (<div>Loading</div>)
     } else {
       let data = this.state.dataJSON.data;
+
+      let employed_count = data.count.employed_count.toString(),
+        killed_count = data.count.killed_count.toString(),
+        convicted_count = data.count.convicted_count.toString(),
+        employed_arr = [],
+        killed_arr = [],
+        convicted_arr = []
+
+      let employed = this.insertZero(employed_count, employed_arr),
+        killed = this.insertZero(killed_count, killed_arr),
+        convicted = this.insertZero(convicted_count, convicted_arr)
       return(
         <div className="ms-cover-mobile">
           <div className="banner-image">
-            <img src={data.banner_image.desktop} height="250px"/>
+            <img src={data.banner_image.mobile} height="250px"/>
           </div>     
           <div className="counter-parent">     
             <div className="m-employed-counter counter">
-              <div className="animate-count m-single-counter m-ec-1">2</div>
-              <div className="animate-count m-single-counter m-ec-2">4</div>
-              <div className="animate-count m-single-counter m-ec-3">0</div>
-              <div className="animate-count m-single-counter m-ec-4">4</div>
+              <div className="animate-count m-single-counter m-ec-1">{employed[0]}</div>
+              <div className="animate-count m-single-counter m-ec-2">{employed[1]}</div>
+              <div className="animate-count m-single-counter m-ec-3">{employed[2]}</div>
+              <div className="animate-count m-single-counter m-ec-4">{employed[3]}</div>
             </div>
             <div className="m-killed-counter counter">
-              <div className="animate-count m-single-counter m-kc-1">0</div>
-              <div className="animate-count m-single-counter m-kc-2">3</div>
-              <div className="animate-count m-single-counter m-kc-3">6</div>
-              <div className="animate-count m-single-counter m-kc-4">6</div>
+              <div className="animate-count m-single-counter m-kc-1">{killed[0]}</div>
+              <div className="animate-count m-single-counter m-kc-2">{killed[1]}</div>
+              <div className="animate-count m-single-counter m-kc-3">{killed[2]}</div>
+              <div className="animate-count m-single-counter m-kc-4">{killed[3]}</div>
             </div>
             <div className="m-convicted-counter counter">
-              <div className="animate-count m-single-counter m-cc-1">0</div>
-              <div className="animate-count m-single-counter m-cc-2">0</div>
-              <div className="animate-count m-single-counter m-cc-3">0</div>
-              <div className="animate-count m-single-counter m-cc-4">2</div>
+              <div className="animate-count m-single-counter m-cc-1">{convicted[0]}</div>
+              <div className="animate-count m-single-counter m-cc-2">{convicted[1]}</div>
+              <div className="animate-count m-single-counter m-cc-3">{convicted[2]}</div>
+              <div className="animate-count m-single-counter m-cc-4">{convicted[3]}</div>
             </div>
           </div>
         </div>
@@ -166,25 +181,24 @@ export default class toStinkCoverVizCard extends React.Component {
     }
   }
 
-  insertZero(count, arr, str) {
-    let len = count.length;
-    for (let i=0; i< len; i++) {
-      switch (len) {
-        case 1:
-          arr.push(0);
-          break;
-        case 2: 
-          arr.push(0);
-          break;
-        case 3:
-          arr.push(0);
-          break;
-        case 4:
-          arr.push(str.toString()[i]);
-          break;
-      }     
+  insertZero(str, arr) {
+    let len = str.length;
+    switch (len) {
+      case 1:
+        arr.unshift(0, 0, 0);
+        break;
+      case 2: 
+        arr.unshift(0, 0);
+        break;
+      case 3:
+        arr.unshift(0);
+        break;
+      case 4:
+        break;
     }
-    console.log(arr, "arr")
+    for (let i=0; i< len; i++) {
+      arr.push(str[i]);
+    }
     return arr;
   }
 
